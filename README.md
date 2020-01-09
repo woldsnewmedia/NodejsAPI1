3fed642da2833680c3ca28f91fde65fd209c45f8
Derek Winters <woldsnewmedia@hotmail.co.uk>
2020-01-09 07:03:06 +0000

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodejsAPI1
+
+
---
+++ i.bat
@@ -0,0 +1,7 @@
+npm install express --save
+npm install jsonwebtoken --save
+npm install bcryptjs --save
+npm install body-parser --save
+npm install cors --save
+npm install mongoose-unique-validator --save
+npm install mongoose --save
